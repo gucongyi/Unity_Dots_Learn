ea6e0926226a4dc79c4d37683ddb6c541c9fee7f
gucongyi <gucongyii@163.com>
2020-06-23 17:05:30 +0800

    第一个ECS功能 LevelUpSystem 每秒+1

---
+++ Unity_Dots_learn/Assets/Scripts/Mono/TestECSMono.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class TestECSMono : MonoBehaviour
+{
+    void Start()
+    {
+        //创建Entity
+        EntityManager entityManager= World.DefaultGameObjectInjectionWorld.EntityManager;
+        //Entity entity=entityManager.CreateEntity(typeof(LevelComponent));
+        Entity entity = entityManager.CreateEntity();
+        //添加对应组件
+        entityManager.AddComponent<LevelComponent>(entity);
+        //给entity的对应组件赋值
+        entityManager.SetComponentData(entity,new LevelComponent() { level=10});
+    }
+}
---
+++ Unity_Dots_learn/Assets/Scripts/System/LevelUpSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+//系统脚本不用挂载，每帧会自动执行OnUpdate
+public class LevelUpSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref LevelComponent levelComponent) => {
+            levelComponent.level += 1f * Time.DeltaTime;
+        });
+    }
+}
