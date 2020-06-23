08f637179207fce9cd7be572a0490db551bedeb7
gucongyi <gucongyii@163.com>
2020-06-23 17:15:44 +0800

    原型方式创建实体

--- Unity_Dots_learn/Assets/Scripts/Mono/TestECSMono.cs
+++ Unity_Dots_learn/Assets/Scripts/Mono/TestECSMono.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
+using Unity.Transforms;
 
 public class TestECSMono : MonoBehaviour
 {
@@ -9,11 +10,18 @@ public class TestECSMono : MonoBehaviour
     {
         //创建Entity
         EntityManager entityManager= World.DefaultGameObjectInjectionWorld.EntityManager;
-        //Entity entity=entityManager.CreateEntity(typeof(LevelComponent));
-        Entity entity = entityManager.CreateEntity();
-        //添加对应组件
-        entityManager.AddComponent<LevelComponent>(entity);
+        ////1.组件方式创建Entity
+        ////Entity entity=entityManager.CreateEntity(typeof(LevelComponent));
+        //Entity entity = entityManager.CreateEntity();
+        ////添加对应组件
+        //entityManager.AddComponent<LevelComponent>(entity);
+
+        //2.原型方式创建Entity
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(typeof(LevelComponent), typeof(Translation));
+        Entity entity = entityManager.CreateEntity(entityArchetype);
+
         //给entity的对应组件赋值
         entityManager.SetComponentData(entity,new LevelComponent() { level=10});
+        entityManager.SetComponentData(entity, new Translation() { Value = new Unity.Mathematics.float3(1f,2f,3f)});
     }
 }
