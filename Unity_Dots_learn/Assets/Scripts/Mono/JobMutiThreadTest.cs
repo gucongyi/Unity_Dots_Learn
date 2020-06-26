ec757d3970b0049e9137c37b58f88c322ea363b4
gucongyii <gucongyii@163.com>
2020-06-26 10:38:40 +0800

    Job多线程完成10个任务，和普通单线程对比

---
+++ Unity_Dots_learn/Assets/Scripts/Mono/JobMutiThreadTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class JobMutiThreadTest : MonoBehaviour
+{
+    [SerializeField]
+    bool useJobs;
+    // Update is called once per frame
+    void Update()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        if (!useJobs)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Helper.ReallyToughTask();
+            }
+        }
+        else
+        {
+            NativeList<JobHandle> jobHandleList = new NativeList<JobHandle>(Allocator.Temp);
+            for (int i = 0; i < 10; i++)
+            {
+                JobHandle jobHandle = ReallyTouchTaskJob();
+                jobHandleList.Add(jobHandle);
+            }
+            //等待所有线程完成
+            JobHandle.CompleteAll(jobHandleList);
+            jobHandleList.Dispose();
+        }
+        
+        float endTime = Time.realtimeSinceStartup;
+        Debug.Log($"Cost Time:{(endTime-startTime)*1000} ms");
+    }
+
+    JobHandle ReallyTouchTaskJob()
+    {
+        ReallyTouchJob job = new ReallyTouchJob();
+        //启动线程执行任务
+        JobHandle jobHandle=job.Schedule();
+        return jobHandle;
+    }
+   
+}
