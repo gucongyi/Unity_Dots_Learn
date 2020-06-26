37e7b983c2ebc16df07030ad1e822322588f0fe5
gucongyii <gucongyii@163.com>
2020-06-26 10:28:36 +0800

    Job 代码块

--- Unity_Dots_learn/Assets/Scripts/Mono/JobTest.cs
+++ Unity_Dots_learn/Assets/Scripts/Mono/JobTest.cs
@@ -1,27 +1,58 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class JobTest : MonoBehaviour
 {
-   
+    [SerializeField]
+    bool useJobs;
     // Update is called once per frame
     void Update()
     {
         float startTime = Time.realtimeSinceStartup;
-        ReallyToughTask();
+        if (!useJobs)
+        {
+            Helper.ReallyToughTask();
+        }
+        else
+        {
+            JobHandle jobHandle = ReallyTouchTaskJob();
+            //等待任务执行完成
+            jobHandle.Complete();
+        }
+        
         float endTime = Time.realtimeSinceStartup;
         Debug.Log($"Cost Time:{(endTime-startTime)*1000} ms");
     }
 
-    private void ReallyToughTask()
+    JobHandle ReallyTouchTaskJob()
+    {
+        ReallyTouchJob job = new ReallyTouchJob();
+        //启动线程执行任务
+        JobHandle jobHandle=job.Schedule();
+        return jobHandle;
+    }
+   
+}
+public static class Helper 
+{
+    public static void ReallyToughTask()
     {
         float value = 0f;
         for (int i = 0; i < 50000; i++)
         {
             value = math.exp10(math.sqrt(value));
         }
+    }
+}
+
+public struct ReallyTouchJob : IJob
+{
+    public void Execute()
+    {
+        Helper.ReallyToughTask();
     }
 }
