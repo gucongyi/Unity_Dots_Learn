d5565a3275113e64210b16a1296210699c7d073b
gucongyii <gucongyii@163.com>
2020-06-26 17:35:51 +0800

    Entities.ForEach uses managed IComponentData MoveDirectionComponent&. This is only supported when using .WithoutBurst() and .Run().
    是因为组件不能是类，必须是struct
    struct MoveDirectionComponent

---
+++ Unity_Dots_learn/Assets/Scripts/System/PlayerMovementSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+public class PlayerMovementSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+        JobHandle jobHandle=Entities.ForEach((ref Translation translation,in MoveDirectionComponent moveDirection) => 
+        {
+            translation.Value.x += moveDirection.Value * deltaTime;
+        }).Schedule(inputDeps);
+        return jobHandle;
+    }
+}
