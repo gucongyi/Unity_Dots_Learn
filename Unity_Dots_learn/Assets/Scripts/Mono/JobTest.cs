44c0b74a2bb0a474a7214a08999a28281e54b6ed
gucongyii <gucongyii@163.com>
2020-06-26 10:21:54 +0800

    执行普通耗时任务

---
+++ Unity_Dots_learn/Assets/Scripts/Mono/JobTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class JobTest : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        ReallyToughTask();
+        float endTime = Time.realtimeSinceStartup;
+        Debug.Log($"Cost Time:{(endTime-startTime)*1000} ms");
+    }
+
+    private void ReallyToughTask()
+    {
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
